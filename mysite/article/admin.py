d1e3e3e230add5a7d6e81c3d562728921bf52cc9
hles61701 <hles61701@gmail.com>
2020-12-18 15:26:56 +0800

    增加model 時間欄位

--- mysite/article/admin.py
+++ mysite/article/admin.py
@@ -5,7 +5,7 @@ from article.models import Article, Comment
 
 
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['Article', 'content']  # 讓清單顯示2欄位
+    list_display = ['Article', 'content', 'pubDateTime']  # 讓清單顯示2+1欄位
     list_display_links = ['Article']  # 設定資料連結欄位
     list_filter = ['Article', 'content']  # 點選文章標題就會列出所屬留言
     search_fields = ['content']  # 出現搜尋欄位
--- mysite/article/models.py
+++ mysite/article/models.py
@@ -6,14 +6,22 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=128, unique=True)
     content = models.TextField()
+    pubDateTime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ['-pubDateTime']
 
 
 class Comment(models.Model):
     Article = models.ForeignKey(Article, on_delete=models.CASCADE)
     content = models.CharField(max_length=128)
+    pubDateTime = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.Article.title + '-' + str(self.id)
+
+    class Meta:
+        ordering = ['pubDateTime']
