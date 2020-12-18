53df50d5eee9da8eceabac05bb66de995d4b26b4
hles61701 <hles61701@gmail.com>
2020-12-18 14:00:51 +0800

    客製化管理者頁面

--- mysite/article/admin.py
+++ mysite/article/admin.py
@@ -3,5 +3,17 @@ from article.models import Article, Comment
 
 # Register your models here.
 
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['Article', 'content']  # 讓清單顯示2欄位
+    list_display_links = ['Article']  # 設定資料連結欄位
+    list_filter = ['Article', 'content']  # 點選文章標題就會列出所屬留言
+    search_fields = ['content']  # 出現搜尋欄位
+    list_editable = ['content']  # 可以直接編輯content欄位內容
+
+    class Meta:
+        model = Comment
+
+
 admin.site.register(Article)
-admin.site.register(Comment)
+admin.site.register(Comment, CommentAdmin)
