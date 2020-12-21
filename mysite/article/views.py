a3bf973a3be21ecd1a47c3abd4a7198837372c83
hles61701 <hles61701@gmail.com>
2020-12-21 23:03:03 +0800

    文章以及留言顯示

--- mysite/article/views.py
+++ mysite/article/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from article.models import Article
+from article.models import Article, Comment
 
 # Create your views here.
 
@@ -8,8 +8,13 @@ def article(request):
     '''
     Render the article page
     '''
-    articles = Article.objects.all()
+
+    # articles = Article.objects.all()
     # 取出所有的文章，Django 稱查詢出來的資料為查詢集(Query set)
+
+    articles = {article: Comment.objects.filter(
+        Article=article) for article in Article.objects.all()}
+    # 將articles 變數改為一個dict K:V=文章:留言
     context = {'articles': articles}
     # 利用範本變數article將查詢集至範本，修改範本以顯示文章資料在context區塊加到內容
     return render(request, 'article/article.html', context)
