2319fc4fd6c192cbcd6588c6feb3fa874514873b
hles61701 <hles61701@gmail.com>
2020-12-21 16:33:54 +0800

    在部落格頁面顯示文章

--- mysite/article/views.py
+++ mysite/article/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from article.models import Article
 
 # Create your views here.
 
@@ -7,4 +8,8 @@ def article(request):
     '''
     Render the article page
     '''
-    return render(request, 'article/article.html')
+    articles = Article.objects.all()
+    # 取出所有的文章，Django 稱查詢出來的資料為查詢集(Query set)
+    context = {'articles': articles}
+    # 利用範本變數article將查詢集至範本，修改範本以顯示文章資料在context區塊加到內容
+    return render(request, 'article/article.html', context)
