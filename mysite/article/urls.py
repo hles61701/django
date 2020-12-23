97c4370bc653303751d46311b90dc178b12d9d55
hles61701 <hles61701@gmail.com>
2020-12-24 03:03:48 +0800

    文章修改

--- mysite/article/urls.py
+++ mysite/article/urls.py
@@ -4,7 +4,12 @@ from article import views
 app_name = 'article'
 urlpatterns = [
     path('', views.article, name='article'),
-    path('articleCreate/', views.articleCreate, name='articleCreate')
+    path('articleCreate/', views.articleCreate, name='articleCreate'),
+    path('articleRead/<int:articleId>/', views.articleRead, name='articleRead'),
+    # <int:articleId> => URL中傳遞資料 int整數:articleId參數名稱
+
+    path('articleUpdate/<int:articleId>/',
+         views.articleUpdate, name='articleUpdate'),
 
 
 ]
--- mysite/article/views.py
+++ mysite/article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from article.models import Article, Comment
 from article.forms import ArticleForm
 from django.contrib import messages
@@ -34,7 +34,7 @@ def articleCreate(request):
     # # TODO: finish the code
     # return render(request, 'article/article.html') # 測試
 
-    template = 'article/articleCreate.html'
+    template = 'article/articleCreateUpdate.html'
     if request.method == 'GET':
         # print(ArticleForm()) 可以看到表單預設結構是table
         return render(request, template, {'articleForm': ArticleForm()})
@@ -50,3 +50,45 @@ def articleCreate(request):
     # return article(request)
     messages.success(request, '文章已新增')
     return redirect('article:article')  # 轉向到article
+
+
+def articleRead(request, articleId):
+    '''
+    Read an article
+        1. Get the article instance; redirect to the 404 page if not found
+        2. Render the articleRead template with the article instance and its associated comments
+    '''
+    article = get_object_or_404(Article, id=articleId)
+    context = {
+        'article': article,
+        # 'comments': Comment.objects.filter(article=article)
+        'comments': Comment.objects.filter(Article=article)
+    }
+    return render(request, 'article/articleRead.html', context)
+
+
+def articleUpdate(request, articleId):
+    '''
+    Update the article instance:
+        1. Get the article to update; redirect to 404 if not found
+        2. If method is GET, render a bound form
+        3. If method is POST,
+            * vaildate the form and render a bound form if the form is invalid
+            * else, save it to the model and redirect to the article page
+    '''
+    # # TODO: finish the code
+    # return render(request, 'article/article.html') 測試
+    article = get_object_or_404(Article, id=articleId)
+    template = 'article/articleCreateUpdate.html'
+    if request.method == 'GET':
+        articleForm = ArticleForm(instance=article)
+        return render(request, template, {'articleForm': articleForm})
+
+    # POST
+    articleForm = ArticleForm(request.POST, instance=article)
+    if not articleForm.is_valid():
+        return render(request, template, {'articleForm': articleForm})
+
+    articleForm.save()
+    messages.success(request, '文章已修改')
+    return redirect('article:articleRead', articleId=articleId)
