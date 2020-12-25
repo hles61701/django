651c826d2b02e5f5599754c9fee84a8080e5098c
hles61701 <hles61701@gmail.com>
2020-12-25 16:58:38 +0800

    會員登入功能

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -3,6 +3,7 @@ from account import views
 
 app_name = 'account'
 urlpatterns = [
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
 
 ]
--- mysite/account/views.py
+++ mysite/account/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import messages
+
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_logon
 
 from account.forms import UserForm
 
@@ -19,4 +22,29 @@ def register(request):
 
     userForm.save()
     messages.success(request, '歡迎註冊')
+    return redirect('main:main')
+
+
+def login(request):
+    '''
+    '''
+    template = 'account/login.html'
+    if request.method == 'GET':
+        return render(request, template)
+
+    # POST
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    if not username or not password:  # Server-side validation
+        messages.error(request, '請填資料')
+        return render(request, template)
+
+    user = authenticate(username=username, password=password)
+    if not user:  # authentication fails
+        messages.error(request, '登入失敗')
+        return render(request, template)
+
+    # login success
+    auth_logon(request, user)
+    messages.success(request, '登入成功')
     return redirect('main:main')
